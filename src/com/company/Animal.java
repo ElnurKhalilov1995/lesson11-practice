6871c9c556e0e5db9952144be59a64fa1a56a23b
ehkhalilov <elnur.khalilov@pashabank.az>
2022-12-09 20:19:26 +0400

    added lesson code

---
+++ src/com/company/Animal.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Animal {
+
+    final void m() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Animal animal = new Dog();
+
+       Cat cat = (Cat) animal;
+    }
+}
